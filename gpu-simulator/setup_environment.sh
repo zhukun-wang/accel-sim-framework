0a6a652e214c31dcc79873e0010c894e3ee590ee
Zhukun Wang <zhukun.wang@chch.ox.ac.uk>
2025-05-21 03:05:20 +0000

    First update

--- gpu-simulator/setup_environment.sh
+++ gpu-simulator/setup_environment.sh
@@ -31,6 +31,9 @@ export ACCELSIM_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 #   Different branches of Accel-Sim should have different values here
 #   For development, we use our internal repo and the dev branch
 #       Ideally, when we release, it should be based off a GPGPU-Sim release.
+export GPGPUSIM_REPO=https://github.com/zhukun-wang/gpgpu-sim_distribution.git
+export GPGPUSIM_BRANCH=memory
+
 export GPGPUSIM_REPO="${GPGPUSIM_REPO:=https://github.com/accel-sim/gpgpu-sim_distribution.git}"
 export GPGPUSIM_BRANCH="${GPGPUSIM_BRANCH:=dev}"
 
